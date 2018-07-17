17338290c446ad0f06f062977b341bfe33818b7c
Christopher Jones <cjones999983@gmail.com>
2018-07-17 01:53:00 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,8 @@ ENV['SINATRA_ENV'] ||= "development"
 ENV['RACK_ENV'] ||= "development"
 
 require 'bundler/setup'
+require "sinatra/activerecord"
+
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 require_all 'app'
