ec79bea102d9233fa4d3b58ac825fea6ca138ff4
Christopher Jones <cjones999983@gmail.com>
2018-07-17 03:40:09 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,2 @@
 class Dog < ActiveRecord::Base
-  def initialize(name, breed, age)
-    Dog.create(name: name, breed: breed, age: age)
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_07_17_033844) do
+
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.string "breed"
+    t.integer "age"
+  end
 
 end
