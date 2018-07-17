d70ef19d1ca0b0bbc681c3652abb8eba249a81bd
Christopher Jones <cjones999983@gmail.com>
2018-07-17 01:53:14 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,8 @@ require "sinatra/activerecord"
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 require_all 'app'
+
+Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+
+connection_details = YAML::load(File.open('config/database.yml'))
+ActiveRecord::Base.establish_connection(connection_details)
